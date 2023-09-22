42f08ed1fd03348a21c1dc8b31276e75f6e7d5f8
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-09-22 01:51:34 +0000

    removed excess styling, columns and rows

--- src/front/js/pages/calendar.js
+++ src/front/js/pages/calendar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 import "../../styles/schedule.css";
+import "../../../../data.json";
 
 
 export const Calendar = () => {
@@ -20,22 +21,72 @@ export const Calendar = () => {
             </li>
             </ul>
             <div className="tab-content" id="myTabContent">
-            {/* Page View */}
             <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
-                <div className="event-panel ep1">
-                    {/* <image className="event-banner event-onomatopoeia">POW</image> */}
-                    <div className="event-banner">Dummy Title</div>
-                    <div className="event event-date">Dummy Date</div> 
-                    {/* why are the styles not applying to the date */}
-                    <div className="event event-start-time">Dummy Start</div>
-                    <div className="event event-end-time">Dummy End</div>
-                    <div className="event event-location">Dummy Location</div>
-                    <div className="event event-description">Dummy Desc</div>
+            {/* Page View */}
+            <div class="container">
+                <div class="row-odd">
+                    <div class="col-2">
+                    One of three columns
+                    </div>
+                    <div class="col-8">
+                        <div className="event-panel ep1">
+                            {/* <image className="event-banner event-onomatopoeia">POW</image> */}
+                            <div className="event-banner">Dummy Title</div>
+                            <div className="event event-date">Dummy Date</div> 
+                            <div className="event event-start-time">Dummy Start</div>
+                            <div className="event event-end-time">Dummy End</div>
+                            <div className="event event-location">Dummy Location</div>
+                            <div className="event event-description">Dummy Desc</div>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                    Three of three columns
+                    </div>
                 </div>
+            </div>
             </div>
             {/* Table View */}
             <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabIndex="0">
-                2
+                <div class="row row-odd">
+                    <div class="col-2">
+                        Dummy Date
+                    </div>
+                    <div class="col-2">
+                        Dummy Start
+                    </div>
+                    <div class="col-2">
+                        Dummy End
+                    </div>
+                    <div class="col-2">
+                        Dummy Title
+                    </div>
+                    <div class="col-2">
+                        Dummy Location
+                    </div>
+                    <div class="col-2">
+                        Dummy Description
+                    </div>
+                </div>
+                <div class="row row-even">
+                    <div class="col-2">
+                        Dummy Date
+                    </div>
+                    <div class="col-2">
+                        Dummy Start
+                    </div>
+                    <div class="col-2">
+                        Dummy End
+                    </div>
+                    <div class="col-2">
+                        Dummy Title
+                    </div>
+                    <div class="col-2">
+                        Dummy Location
+                    </div>
+                    <div class="col-2">
+                        Dummy Description
+                    </div>
+                </div>
             </div>
             {/* <div>{store.events.map((event_name, index) => <Card key={index} event_name={event_name} index={index}/>)}</div> */}
             </div>
