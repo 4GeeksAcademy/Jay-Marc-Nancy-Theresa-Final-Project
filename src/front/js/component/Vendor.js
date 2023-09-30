cf388f0897f1fbd51f4293c7f97b173c454267b4
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-09-30 00:44:07 +0000

    Vendor component prototype built

--- src/front/js/component/Vendor.js
+++ src/front/js/component/Vendor.js
@@ -1,52 +1,59 @@
-// import React, { useContext } from "react";
-// import PropTypes from "prop-types";
-// import { Link } from "react-router-dom";
-// import { Context } from "../store/appContext";
-// import data from "../../../../data.json";
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import data from "../../../../data.json";
 
-// const Vendor = (props) => {
-//     const {store, actions} = useContext(Context);
+const Vendor = (props) => {
+    const { store, actions } = useContext(Context);
+    useEffect(() => {
+        actions.getArtVendors();   
+        actions.getMerchVendors();
+    })
 
-//     let field
-//     props.type === "comicVendor" ? (
-//         field = 
-//             <ul className="list-group">
-//                 {store.comicVendors.map((vendor, idx) =>
-//                 <li key={idx} className="row list-group-item">
-//                     <div  className="col li-vendor-name">
-//                         {vendor.name}
-//                     </div>
-//                 </li>
-//             </ul>
-//     ) : null
+    let field
+    props.type === "comicVendor" ? (
+        field = 
+            <ul className="list-group">
+            {store.comicVendors.map((vendor, idx) =>
+                <a key={idx} href={vendor.site_url} target="_blank">
+                    {vendor.name}
+                </a>)
+            }
+            </ul>
+    ) : null
 
-//     props.type === "artVendor" ? (
-//         field = 
-//         <ul class="list-group">
-//             <li class="row list-group-item">
-//                 <div class="col li-vendor-name">ARTVENDORNAME</div>
-//             </li>
-//         </ul>
-//     ) : null
+    props.type === "artVendor" ? (
+        field = 
+        <ul className="list-group">
+            {data.art_vendors.map((artVendor, index) => 
+                <a key={index}>
+                    {artVendor.vendor_name}
+                </a>)
+            }
+        </ul>
+    ) : null
 
-//     props.type === "merchVendor" ? (
-//         field = 
-//         <ul class="list-group">
-//             <li class="row list-group-item">
-//                 <div class="col">MERCHVENDORNAME</div>
-//             </li>
-//         </ul>
-//     ): null
+    props.type === "merchVendor" ? (
+        field = 
+        <ul className="list-group">
+            {data.merch_vendors.map((merchVendor, index) => 
+                <a key={index}>
+                    {merchVendor.vendor_name}
+                </a>)
+            }
+        </ul>
+    ): null
     
-//     return (
-//         <>
-//             {field}
-//         </>
-//     );
+    return (
+        <>
+            {field}
+        </>
+    );
 
-//     Vendor.prototype = {
-//         name: PropTypes.string
-//     };
-// };
+    Vendor.prototype = {
+        name: PropTypes.string
+    };
+};
 
-// export default Vendor;
+export default Vendor;
