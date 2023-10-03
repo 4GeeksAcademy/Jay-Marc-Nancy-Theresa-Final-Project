66ac29128333a9d6d75b2da6e545e18ecef49f50
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-10-03 00:47:35 +0000

    404 error on GET

---
+++ src/front/js/pages/FAQ.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/FAQ.css";
+
+export const FAQ = () => {
+	const { store, actions } = useContext(Context);
+    
+    useEffect(() => {
+        actions.getFAQData();   
+    })
+
+	return (
+        <div className="container">
+            {/* make jumbotron a component for this file, schedule, and vendors */}
+            <div className="FAQJumbotron bg-light p-5 rounded-lg m-3">
+                <img src="https://images.unsplash.com/photo-1477281765962-ef34e8bb0967?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1933&q=80"></img>
+                <div className="jumbotronContent">
+                    <h1 className="display-4">FAQ</h1>
+                    <p className="lead">Have Questions? We Have Answers!</p>
+                    <hr className="my-4"></hr>
+                    <p>Got questions about the Worst Con EverCon? We do too. You're in the right place. We've gathered all the information you need to make your experience smooth, enjoyable, and filled with epic moments. If you can't find what you're looking for here, feel free to reach out to our support team. Your adventure begins here!</p>
+                </div>
+            </div>
+            <div className="accordion-wrapper">
+                {store.faq_data.map((faq_data, index) => 
+                    <div className="row accordion-event" key={index}>
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id={faq_data.id}>
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={String('#collapsebox' + faq_data.id)} aria-expanded="false" aria-controls={faq_data.id}>
+                                    {faq_data.question}
+                                </button>
+                                </h2>
+                                <div id={String('collapsebox' + faq_data.id)} className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <div className="full-accordion">
+                                            {faq_data.answer}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,18 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			faq_data: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -33,20 +22,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			getFAQData: () => {
+				fetch("../../../../data.json") 
+				.then((resp) => resp.json())
+				.then((data) => {
+					console.log("myString: ", data)
+					setStore({ faq_data: data.faq_data })
+				} )
+				//console log data
+		},		
 		}
 	};
 };
