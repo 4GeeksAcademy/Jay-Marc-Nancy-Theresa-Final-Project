09374cd7b315ff6cba29cf2bc14a20af097c0590
Nancy J Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-10-04 03:02:02 +0000

    requirements met, img successfully rendered from JSON file

--- src/front/js/pages/newReleases.js
+++ src/front/js/pages/newReleases.js
@@ -16,29 +16,32 @@ export const NewReleases = () => {
                 </div>
             </div>
             <div className="row">
-                <div className="col-12">
-                    <div className="card-deck">
-                        {store.comics.map((item, index) => {
-                            return (
-                                <div className="card" key={index}>
-                                    {/* <img src={item.image} className="card-img-top" alt="..." /> */}
-                                    <div className="card-body">
-                                        <h5 className="card-title">{item.title}</h5>
-                                        <p className="card-text">{item.publisher}</p>
-                                        <p className="card-text">{item.description}</p>
-                                        <p className="card-text">{item.price}</p>
-                                        <p className="card-text">{item.creators}</p>
-                                        <p className="card-text">{item.release_date}</p>
-                                        <p className="card-text">{item.diamond_id}</p>
-                                        <p className="card-text">
-                                            {/* <small className="text-muted">{item.publisher}</small> */}
-                                        </p>
+                {/* <div className="col-12"> */}
+                <div className="card-comics">
+                    {store.comics.map((item, index) => {
+                        return (
+                            <div className="row no-gutters">
+                                <div className="col auto" key={index}>
+                                    <div className="card-header">
+                                        <img src={item.img_url} className="card-img-top" alt="..." id="newComicsImg" />
+                                    </div>
+                                    <div className="col">
+                                        <div className="card-block px-2">
+                                            <h5 className="card-title"> {item.title}</h5>
+                                            <p className="card-text">Publisher: {item.publisher}</p>
+                                            <p className="card-text">Description: {item.description}</p>
+                                            <p className="card-text">Price: {item.price}</p>
+                                            <p className="card-text">Creators: {item.creators}</p>
+                                            <p className="card-text">Release date: {item.release_date}</p>
+                                            <p className="card-text">Diamond Id: {item.diamond_id}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            );
-                        })}
-                    </div>
+                            </div>
+                        );
+                    })}
                 </div>
+                {/* </div> */}
             </div>
         </div>
     );
