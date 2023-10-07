92a5ff749cb18abec34056232be781e2409c33ef
labs404 <136141267+labs404@users.noreply.github.com>
2023-10-07 01:32:21 +0000

    initial commit post pull origin main

--- src/api/models.py
+++ src/api/models.py
@@ -32,11 +32,11 @@ class User(db.Model):
 class Magic(db.Model):
     __tablename__ = 'Magic'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False),
-    type = db.Column(db.String(80), unique=False, nullable=False),
-    description = db.Column(db.String(120), unique=False, nullable=False),
-    level = db.Column(db.String(80), unique=False, nullable=False),
-
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    type = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    level = db.Column(db.String(80), unique=False, nullable=False)
+    
     def __repr__(self):
         return f'<Magic {self.id}>'
 
