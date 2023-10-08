5fccb289e906b1ccb311aba7a7ac4cc00ee8caae
Nancy J Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-10-08 22:14:04 +0000

    styled login, signup, home, new releases

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 import "../../styles/login.css";
+import deadpool from "../../img/deadpool.jpg";
 
 export const Login = () => {
     const { store, actions } = useContext(Context);
@@ -23,36 +24,45 @@ export const Login = () => {
     }
     return (
         <>
-            <div className="login-box">
-                {(store.token && store.token !== "" && store.token !== undefined) ? "You are logged in with token"
-                    :
-                    <div align="center">
-                        <h1>Login</h1>
-                        <div className="user-box">
-                            <input
-                                type="text"
-                                value={email}
-                                placeholder="enter email" onChange={e => setEmail(e.target.value)}></input>
+            <div className="login-wrapper">
+                <div className="login-box">
+                    <div className="row">
+                        <div className="col-6">
+                            <img src={deadpool} className="loginImg" alt="avatar" />
                         </div>
-                        <div className="user-box">
-                            <input
-                                type="password"
-                                value={password}
-                                placeholder="enter password" onChange={e => setPassword(e.target.value)}></input>
+                        <div className="col-6">
+                            {(store.token && store.token !== "" && store.token !== undefined) ? "You are logged in with token"
+                                :
+                                <div align="center">
+                                    <h1>Login</h1>
+                                    <div className="user-box my-3">
+                                        <input
+                                            type="text"
+                                            value={email}
+                                            placeholder="Email" onChange={e => setEmail(e.target.value)}></input>
+                                    </div>
+                                    <div className="user-box my-3">
+                                        <input
+                                            type="password"
+                                            value={password}
+                                            placeholder="Password" onChange={e => setPassword(e.target.value)}></input>
+                                    </div>
+                                    <button className="loginPageButton" onClick={handleLogin}>Login
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </button>
+
+
+
+                                    {/* <button onClick={() => navigate("/signup")}>Signup</button> */}
+                                    {/* <button onClick={() => navigate("/forgot-password")}>Forgot Password</button> */}
+                                </div>
+                            }
                         </div>
-                        <button onClick={handleLogin}>Login
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </button>
-
-
-
-                        {/* <button onClick={() => navigate("/signup")}>Signup</button> */}
-                        {/* <button onClick={() => navigate("/forgot-password")}>Forgot Password</button> */}
                     </div>
-                }
+                </div>
             </div>
         </>
     )
