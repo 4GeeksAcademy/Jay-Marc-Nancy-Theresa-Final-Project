c427cd3e9102a20274b611661b22ebda0c8e760b
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-10-11 23:38:53 +0000

    styling

--- src/front/js/pages/schedule.js
+++ src/front/js/pages/schedule.js
@@ -23,12 +23,12 @@ export const Schedule = () => {
             </div>
             <ul className="nav nav-tabs" id="myTab" role="tablist">
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link eventNavLink text-secondary active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">
+                    <button className="nav-link eventNavLink active button-53" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">
                         Favorites
                     </button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link eventNavLink text-secondary" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">
+                    <button className="nav-link eventNavLink button-53" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">
                         Table
                     </button>
                 </li>
@@ -62,13 +62,13 @@ export const Schedule = () => {
                                             <div className="favoritesButtonContainer">
                                                 { isFavorite ? (
                                                     <button className="eventFavoriteButton button-53" onClick={() => actions.deleteFavorite(event.id)}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill font-spidey-darkBlue" viewBox="0 0 16 16">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart-fill font-spidey-darkBlue" viewBox="0 0 16 16">
                                                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                                                         </svg>
                                                     </button>
                                                 ) : (
                                                     <button className="eventFavoriteButton button-53" onClick={() => actions.addFavorite(event)}>
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart font-spidey-darkBlue" viewBox="0 0 16 16">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart font-spidey-darkBlue" viewBox="0 0 16 16">
                                                             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                                                         </svg>
                                                     </button>
@@ -123,10 +123,10 @@ export const Schedule = () => {
                                         </h2>
                                         <div id={String('collapsebox' + event.id)} className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                             <div className="accordion-body">
-                                                <div className="col-2 full-accordion">
+                                                <div className="full-accordion">
                                                     {event.location}
                                                 </div>
-                                                <div className="col-6 full-accordion full-accordion-description">
+                                                <div className=" full-accordion full-accordion-description">
                                                     {event.description}
                                                 </div>
                                             </div>
