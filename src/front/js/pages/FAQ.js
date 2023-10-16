b1af651dddd7afbd2b4e2c3d5bcc9a6c32a09cb4
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-10-16 19:31:05 +0000

    FAQ mapping, but separately

--- src/front/js/pages/FAQ.js
+++ src/front/js/pages/FAQ.js
@@ -22,28 +22,68 @@ export const FAQ = () => {
                     <p>Got questions about the Worst Con EverCon? We do too. You're in the right place. We've gathered all the information you need to make your experience smooth, enjoyable, and filled with epic moments. If you can't find what you're looking for here, feel free to reach out to our support team. Your adventure begins here!</p>
                 </div>
             </div>
-            <div className="accordion-wrapper">
-                {data.faq_data.map((faq_data, index) => 
-                    <div className="row accordion-event" key={index}>
-                        <div className="accordion" id="accordionExample">
-                            <div className="accordion-item">
-                                <h2 className="accordion-header" id={faq_data.id}>
-                                <button className="accordion-button FAQAccordionButton collapsed" type="button" data-bs-toggle="collapse" data-bs-target={String('#collapsebox' + faq_data.id)} aria-expanded="false" aria-controls={faq_data.id}>
-                                    {faq_data.question}
-                                </button>
-                                </h2>
-                                <div id={String('collapsebox' + faq_data.id)} className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                    <div className="accordion-body">
-                                        <div className="full-accordion">
-                                            {faq_data.answer}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+            {/* accordion for faq */}
+            <div className="col-12 text-start mt-3">
+            {/* div link header to open/close collapsable content */}                
+            {data.faq_data.map((faq_data, index) => 
+                <a type="button" className="container button-55 bgLightBlue borderYellow mt-3" data-bs-toggle="collapse" href="#how-we-got-started-collapse-component" role="button" aria-expanded="false" aria-controls="how-we-got-started-collapse-component">
+                <div className="container text-start">
+                    <div className="row">
+                        <div className="col-9 text-start text-uppercase fs2p0 badaboom font-spidey-yellow textBorderBlack align-self-center">
+                            {faq_data.question}
                         </div>
+                        {/* <div className="col-3 text-end font-spidey-yellow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-chevron-expand" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z" />
+                            </svg>
+                        </div> */}
                     </div>
+                </div>
+            </a>
                 )}
-            </div>
+             {/* collapsable content */}
+             <div className="container-fluid">
+             {data.faq_data.map((faq_data, index) => 
+             <div className="row">
+                 <div className="col aboutDropdownSpacer">                                    
+                 </div>
+                 <div className="m-0 p-0">
+                     <div className="collapse mb-3" id="how-we-got-started-collapse-component">
+                         <div className="card card-body bgLightestYellow mt-2 font-spidey-darkBlue">
+                             {faq_data.answer}
+                         </div>
+                     </div>
+                 </div>
+                 <div className="col aboutDropdownSpacer">                                                                  
+                 </div>
+             </div>
+             )}
+         </div>
+
+
+
+
+
+
+                    {/* // <div className="row accordion-event" key={index}>
+                    //     <div className="accordion" id="accordionExample">
+                    //         <div className="accordion-item">
+                    //             <h2 className="accordion-header" id={faq_data.id}>
+                    //             <button className="accordion-button FAQAccordionButton collapsed" type="button" data-bs-toggle="collapse" data-bs-target={String('#collapsebox' + faq_data.id)} aria-expanded="false" aria-controls={faq_data.id}>
+                    //                 {faq_data.question}
+                    //             </button>
+                    //             </h2>
+                    //             <div id={String('collapsebox' + faq_data.id)} className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    //                 <div className="accordion-body">
+                    //                     <div className="full-accordion">
+                    //                         {faq_data.answer}
+                    //                     </div>
+                    //                 </div>
+                    //             </div>
+                    //         </div>
+                    //     </div>
+                    // </div> */}
         </div>
+    </div>
     )
 }       
